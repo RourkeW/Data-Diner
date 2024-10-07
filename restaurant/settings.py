da90d820a2f33812c3eac8df6fedc186567938a6
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-07 19:41:40 +0000

    feat: I have now connected the postgres database successfully

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +30,8 @@ SECRET_KEY = 'django-insecure-=mu8zg2$7q-!jhs-w$9d^_(&yk8$7^_wsy@+s#8y8$9^i4oym6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rourkew-datadiner-xqxhg86p0jm.ws-eu116.gitpod.io/']
+ALLOWED_HOSTS = ['8000-rourkew-datadiner-xqxhg86p0jm.ws-eu116.gitpod.io/'
+                ,'.herokuapp.com']
 
 
 # Application definition
@@ -74,11 +80,15 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
