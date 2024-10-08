c708eb5b4f619f005fa9597c981dd6651cc031d4
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-08 02:31:16 +0000

    feat: Creation of the meals model is complete, I have used the django prompts to also download a new package called Pillow

--- booking/models.py
+++ booking/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Meals(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField(max_length=250)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='meals/')
 
-class BookTable(models.Model):
-    table_number = models.IntegerField(unique=true)
-    capacity = models.IntegerField()
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,8 +29,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rourkew-datadiner-xqxhg86p0jm.ws-eu116.gitpod.io/'
-                ,'.herokuapp.com']
+ALLOWED_HOSTS = ['https://8000-rourkew-datadiner-xqxhg86p0jm.ws-eu116.gitpod.io/'
+                'herokuapp.com',]
 
 
 # Application definition
@@ -80,21 +79,15 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
+#DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-# }
-
+#}
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeanyapp.com",
-    "https://*.herokuapp.com"
-]
 
 
 # Password validation
