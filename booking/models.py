81bcfd1c9186904487c0921abe513ede8eef3ca2
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-08 01:50:51 +0000

    issue: I have attempted to update the database with the model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BookTable(models.Model):
+    table_number = models.IntegerField(unique=true)
+    capacity = models.IntegerField()
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=mu8zg2$7q-!jhs-w$9d^_(&yk8$7^_wsy@+s#8y8$9^i4oym6'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,6 +90,11 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
