58bbaa84f54eca05bb5fd3d5785fe0a32c4e1245
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-09 14:01:01 +0000

    feat:creation of slug field for Meals model

--- booking/models.py
+++ booking/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+
+
 class Meals(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(max_length=250)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
 
