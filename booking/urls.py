e55761e0c666a78327030621d482ac84cdb6c035
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-14 00:32:02 +0000

    feat; I have added the views

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
-app_name = 'booking'  # Set the app_name here
+handler404 = 'booking.views.custom_404'
 
 urlpatterns = [
-    path('', views.meal_list),
-    path('summernote/', include('django_summernote.urls')),
-    path('<slug:slug>' , views.meal_detail , name='meal_detail')
+    path('', views.BookingList.as_view(), name='home'),
+    path('menu/', views.menu, name='menu'),
+    path('bookinglist/', views.BookListView.as_view(), name='bookinglist'),
+    path('<pk>/delete/', views.BookDeleteView.as_view(), name='delete'),
+    path('<pk>/update/', views.BookUpdateView.as_view(), name='bookingupdate'),
+    path('bookform/', views.BookFormView.as_view(), name='bookform'),
+    path('custom_404/', views.custom_404, name='custom_404'),
 ]
