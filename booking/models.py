9c72d4fc00a658b32f0249be9a69f6c02414b372
Rourke Whitcombe <rourke28whit@gmail.com>
2024-10-14 00:28:04 +0000

    feat: I have added a model for the booking form

--- booking/models.py
+++ booking/models.py
@@ -1,43 +1,50 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 
-class Meals(models.Model):
-    name = models.CharField(max_length=25)
-    description = models.TextField(max_length=250)
-    category = models.ForeignKey('Category' , on_delete=models.SET_NULL, null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='meals/')
-    slug = models.SlugField(blank=True, null=True)
-
-# override the save function for Meals model and say if there is a name without a slug then slugify name and add to slug
-    def save(self, *args, **kwargs):
-        if not self.slug and self.name :
-            self.slug = slugify(self.name)
-        super(Meals , self).save(*args , **kwargs)
-
-
-    class Meta:
-        verbose_name = 'meal'
-        verbose_name_plural = 'meals'
+class Booking(models.Model):
+    STATUS_CHOICES = (
+        ("Pending", "Pending"),
+        ("Confirmed", "Confirmed"),
+        ("Cancelled", "Cancelled"),
+    )
+    TIME_CHOICES = [
+        ('09:00', '9:00 AM'),
+        ('09:30', '9:30 AM'),
+        ('10:00', '10:00 AM'),
+        ('10:30', '10:30 AM'),
+        ('11:00', '11:00 AM'),
+        ('11:30', '11:30 AM'),
+        ('13:00', '1:00 PM'),
+        ('13:30', '1:30 PM'),
+        ('14:00', '2:00 PM'),
+        ('14:30', '2:30 PM'),
+        ('15:00', '3:00 PM'),
+        ('15:30', '3:30 PM'),
+        ('16:00', '4:00 PM'),
+        ('16:30', '4:30 PM'),
+        ('18:00', '6:00 PM'),
+        ('18:30', '6:30 PM'),
+        ('19:00', '7:00 PM'),
+        ('19:30', '7:30 PM'),
+        ('20:00', '8:00 PM'),
+    ]
+    guest = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.CharField(max_length=5, choices=TIME_CHOICES)
+    group = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(15)])
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+    is_cancelled = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=30)
-
+        return f"{self.guest} - {self.date} {self.time}"
 
     class Meta:
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-
-    def __str__(self):
-        return self.name
+        ordering = ["created_on"]
 
     
 
